8e2b41a54bb209f812102de415808b86f0bd1049
descreetStudios <descreetstudios@gmail.com>
2025-05-29 10:56:44 +0200

    feat: message card component; fix login onSubmit duplicate call

--- pages/login.vue
+++ pages/login.vue
@@ -1,56 +1,61 @@
 <template>
-	<main class="login-page">
-		<section class="login-card">
-			<img
-				src="assets/images/logo/gamefier-logo-64px.png"
-				alt="L"
-				class="logo"
-			>
-			<h1 class="login-title">
-				Login
-			</h1>
-			<form
-				class="login-form"
-				@submit.prevent="onSubmit"
-			>
-				<input
-					v-model="email"
-					type="email"
-					placeholder="Email"
-					required
-					autocomplete="email"
-				>
-				<input
-					v-model="password"
-					type="password"
-					placeholder="Password"
-					required
-					autocomplete="current-password"
-				>
-				<button
-					type="submit"
-					class="btn-login"
-					@click="onSubmit"
-				>
-					Login
-				</button>
-			</form>
-			<button
-				class="btn-google"
-				@click="onGoogleLogin"
-			>
+	<div>
+		<AppMessageCard>
+			<p>This is a message that comes from the top!</p>
+		</AppMessageCard>
+
+		<main class="login-page">
+			<section class="login-card">
 				<img
-					src="assets/images/icons/google-icon.svg"
-					alt="G"
-					class="google-icon"
+					src="assets/images/logo/gamefier-logo-64px.png"
+					alt="L"
+					class="logo"
 				>
-				Login with Google
-			</button>
-			<footer class="login-footer">
-				Don't have an account? <NuxtLink to="/signup">Sign up</NuxtLink>
-			</footer>
-		</section>
-	</main>
+				<h1 class="login-title">
+					Login
+				</h1>
+				<form
+					class="login-form"
+					@submit.prevent="onSubmit"
+				>
+					<input
+						v-model="email"
+						type="email"
+						placeholder="Email"
+						required
+						autocomplete="email"
+					>
+					<input
+						v-model="password"
+						type="password"
+						placeholder="Password"
+						required
+						autocomplete="current-password"
+					>
+					<button
+						type="submit"
+						class="btn-login"
+					>
+						Login
+					</button>
+				</form>
+				<button
+					class="btn-google"
+					@click="onGoogleLogin"
+				>
+					<img
+						src="assets/images/icons/google-icon.svg"
+						alt="G"
+						class="google-icon"
+					>
+					Login with Google
+				</button>
+				<footer class="login-footer">
+					Don't have an account? <NuxtLink to="/signup">Sign up</NuxtLink>
+				</footer>
+			</section>
+		</main>
+	</div>
 </template>
 
 <script setup>
